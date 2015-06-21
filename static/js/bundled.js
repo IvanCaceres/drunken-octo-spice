8402ebf3ae6fe036e41d70bf2d0d0858c6ca8a13
Ivan Caceres <ivanny109@gmail.com>
2015-06-21 15:33:28 -0400

    details page form steps validation donex

--- static/js/bundled.js
+++ static/js/bundled.js
@@ -57449,7 +57449,16 @@ module.exports = function($scope,$stateParams,AddressDetail, CarYears, CarModels
     console.log($scope)
     console.log($stateParams)
     console.log($scope.$parent.map)
+    $scope.data = {
+    	services: [],
+    	first_name: '',
+    	last_name: '',
+    	phone: '',
+    	email: '',
+    	description: ''
+    };
 	$scope.hours = [1,2,3,4,5,6,7,8,9,10,11,12];
+	$scope.currentStep = 1;
 	$scope.steps = [
 					{
 						id:1,
@@ -57483,28 +57492,25 @@ module.exports = function($scope,$stateParams,AddressDetail, CarYears, CarModels
 	});
 
 	$scope.clickHour = function (hour) {
-		if($scope.hourSelected == hour){
-			$scope.hourSelected = null;
+		console.log('clicking hourrrrr');
+		if($scope.data.hourSelected == hour){
+			$scope.data.hourSelected = null;
 		} else {
-			$scope.hourSelected = hour;
+			$scope.data.hourSelected = hour;
 		}
 	};
 
-	$scope.collapseDropdowns = function (e) {
-		console.log('you are clicking collapse DD', e);
+	$scope.collapseDropdown = function (step) {
+		console.log('you are clicking collapse DD', step, $scope.currentStep);
+		if (step.id <= $scope.currentStep && step.isCollapsed == true) {
+			console.log('is it trigginggg?G???');
+			step.isCollapsed = false;				
+		} else if (step.id <= $scope.currentStep && step.isCollapsed == false) {
+			step.isCollapsed = true;
+		}
 	}
 
-	//watch for CarMake & Year dropdown changes
-	// $scope.$watch('CarMake', function() {
-	// 	console.log(' car make watcherrr!!!!');
-	// 	$scope.updateCarModels()
-	// });
-	// $scope.$watch('CarYear', function() {
-	// 	console.log(' car yearrrr watcherr!!!!');
-	// 	updateCarModels()
-	// });
-
-	$scope.updateCarModels = function (){
+	$scope.updateCarModels = function(){
 		console.log('CarMake:')
 		console.log($scope.CarMake)
 		console.log('CarYear:')
@@ -57516,6 +57522,110 @@ module.exports = function($scope,$stateParams,AddressDetail, CarYears, CarModels
 				});
 		}
 	};
+
+	$scope.saveCarSelection= function(){
+		console.log('saving car selection!');
+		if ($scope.CarModel) {
+			$scope.data.carModel = $scope.CarModel;
+		}
+	}
+
+	$scope.deleteCarSelection = function () {
+		delete $scope.data.carModel;
+	}
+
+	$scope.nextStep = function (step) {
+		console.log('show scope', $scope, step)
+		if ($scope.validateStep(step.id)){
+			step.isCollapsed = true;
+			var nextStepScope = angular.element('.section-container:eq(' + step.id +')').scope();
+			nextStepScope.step.isCollapsed = false;
+			$scope.currentStep = step.id + 1;
+			console.log('show me next step scope', nextStepScope);
+		}
+	}
+
+	$scope.validateStep = function(stepId) {
+		switch (stepId) {
+			case 1:
+				if ($scope.data.date){
+
+				} else {
+					alert('Please select a Day.');
+					break;
+				}
+				if ($scope.data.hourSelected) {
+
+				} else {
+					alert('Please select an Hour');
+					break;
+				}
+				return true;
+			break;
+			case 2:
+				if ($scope.data.services.length > 0){
+
+				} else {
+					alert('Please choose a service');
+					break;
+				}
+				return true;
+			break;
+			case 3:
+				if ($scope.data.carModel){
+
+				} else {
+					alert('Please save a car');
+					break;
+				}
+				return true;
+			break;
+			case 4:
+				if ($scope.data.first_name.length > 1){
+
+				} else {
+					alert('please enter first name');
+					break;
+				}
+				if ($scope.data.last_name.length > 1){
+					
+				} else {
+					alert('please enter last name');
+					break;
+				}
+				if ($scope.data.phone.length >= 7){
+
+				} else {
+					alert('please enter a phone number');
+					break;
+				}
+				if ($scope.data.email.length > 1){
+				} else {
+					alert('please enter an email');
+					break;
+				}
+				if ($scope.data.description.length > 1){
+
+				} else {
+					alert('please enter a description');
+					break;
+				}
+				return true;
+			break;	
+		}	
+	};
+
+	$scope.updateServiceSelection = function(service) {
+		console.log('calling update service selection show args', arguments);
+		if(service.selected == true) {
+			$scope.data.services.push(service.id);
+		} else if (service.selected == false) {
+			var index = $scope.data.services.indexOf(service.id);
+			if (index > -1){
+				$scope.data.services.splice(index, 1);
+			}
+		}
+	}
 
 	if($scope.$parent.map.stores.$promise){
 		console.log('we found the map stores ');
